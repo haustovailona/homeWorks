f28c5a253b5b64521a93963afe1f6bc0537de519
Ilona <hausttovailona@mail.ru>
2024-05-16 14:31:02 +0300

    first task

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        String[] fruits = {"Orange", "Banana", "Apple"};
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+    }
 }
